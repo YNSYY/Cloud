bf943e9b6bfab9066dcc56cf6a4e1403863a0cb3
YNSYY <42941012+YNSYY@users.noreply.github.com>
2018-12-20 15:46:31 +0800

    Delete 07.js

--- 1/07.js
+++
@@ -1,9 +0,0 @@
-//创建一个数组，包含5个车名，单独添加2个车名，最后遍历数组。
-var arr=['玛莎拉蒂','法拉利','保时捷'];
-arr[arr.length]='兰博基尼';
-arr[arr.length]='奔驰';
-console.log(arr);
-for (i=0;i<arr.length;i++)
-{
-	console.log(arr[i]);
-}
